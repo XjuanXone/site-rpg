0b27d67bad88952291a7e9e09b55718c11da379c
XjuanXone <juanrafaelexs@gmail.com>
2024-01-09 22:42:13 -0300

    create Sidebar component

---
+++ siterpg/src/components/sidebar.tsx
@@ -0,0 +1,10 @@
+import { ActionButton } from "./buttons/action";
+
+export function Sidebar() {
+return (
+    <div className="bg-[#ffbd00] h-screen sm:max-w-[300px] w-fit fixed right-0">
+        <ActionButton>
+        </ActionButton>
+    </div>
+)
+}
