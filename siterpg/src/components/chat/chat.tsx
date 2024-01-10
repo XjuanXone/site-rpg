3946c8392684b3c02a10cd6d90858961ec3bbd92
XjuanXone <juanrafaelexs@gmail.com>
2024-01-10 20:16:50 -0300

    create a chat component

---
+++ siterpg/src/components/chat/chat.tsx
@@ -0,0 +1,9 @@
+import { ChatBackground } from "./background";
+
+export function Chat () {
+  return(
+    <ChatBackground src="https://media.tenor.com/BTwfZwYmtToAAAAM/yo-cuando-spinning.gif">
+      <p>Chat</p>
+    </ChatBackground>
+  )  
+}
