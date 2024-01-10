76352dfc1d3ea460a24ced3d4bc6c287962ff3b6
XjuanXone <juanrafaelexs@gmail.com>
2024-01-10 20:18:05 -0300

    update page

--- siterpg/src/app/page.tsx
+++ siterpg/src/app/page.tsx
@@ -1,8 +1,15 @@
 import { DefaultButton } from '@/components/buttons/default'
 import Image from 'next/image'
+import { ChatBackground } from '../components/chat/background';
+import { Chat } from '../components/chat/chat';
 
 export default function Home() {
   return (
-    <main className='font-extrabold'> estamos construindo, volte depois :) </main>
+    <main className='font-extrabold'>
+       estamos construindo, volte depois :) 
+    
+    <Chat/>
+    </main>
+
   )
 }
