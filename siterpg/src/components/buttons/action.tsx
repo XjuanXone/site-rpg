8de73e3b87da8807247a730cf53c2caa2bf4c7e7
XjuanXone <juanrafaelexs@gmail.com>
2024-01-09 22:59:58 -0300

    create button component

---
+++ siterpg/src/components/buttons/action.tsx
@@ -0,0 +1,7 @@
+export function ActionButton(){
+    return (
+        <div className="w-fit h-fit group rounded-lg border border-black bg-blue-400 px-5 py-4 transition-colors hover:bg-yellow-300">
+            <p className="text-2xl font-bold">peixe </p>
+        </div>
+    )
+}
