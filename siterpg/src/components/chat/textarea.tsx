c3cab46e4b060e8cdefec767a30fe9219d20a798
XjuanXone <juanrafaelexs@gmail.com>
2024-01-15 21:47:09 -0300

    create text area compontent

---
+++ siterpg/src/components/chat/textarea.tsx
@@ -0,0 +1,10 @@
+export function TextArea() {
+    return (
+        <div className="w-full absolute bottom-0 left-0 right-0 bg-transparent ">
+            <form className="w-full flex items-center justify-center">
+                <input type="text" placeholder="fala ai pow" className="m-6 mb-1 p-3 w-full"/>
+            </form>
+        </div>
+    )
+    
+}
