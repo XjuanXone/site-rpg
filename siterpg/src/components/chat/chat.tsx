8c6874b9c52b9b496d88d4b3633e40a98aabd3d0
XjuanXone <juanrafaelexs@gmail.com>
2024-01-15 21:50:32 -0300

    add text area on chat

--- siterpg/src/components/chat/chat.tsx
+++ siterpg/src/components/chat/chat.tsx
@@ -1,9 +1,10 @@
 import { ChatBackground } from "./background";
-
+import {TextArea} from './textarea';
 export function Chat () {
   return(
     <ChatBackground src="https://media.tenor.com/BTwfZwYmtToAAAAM/yo-cuando-spinning.gif">
       <p>Chat</p>
+      <TextArea/>
     </ChatBackground>
   )  
 }
